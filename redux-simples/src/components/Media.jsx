fe786d392d43e2dbba5f8cbe8eb7caa78b645a7e
diogo <diogo.lima.flooxai@gmail.com>
2025-04-25 08:20:15 -0300

    feat: add logic using useState without redux

--- redux-simples/src/components/Media.jsx
+++ redux-simples/src/components/Media.jsx
@@ -1,12 +1,13 @@
 import Card from './Card'
 
 export default props => {
+  const {min, max} = props
   return (
     <Card title='Média dos Números' green>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong> 5</strong>
+          <strong> {(max + min ) / 2}</strong>
         </span>
       </div>
     </Card>
--- redux-simples/src/components/Soma.jsx
+++ redux-simples/src/components/Soma.jsx
@@ -1,12 +1,13 @@
 import Card from './Card'
 
 export default props => {
+  const {min, max} = props
   return (
     <Card title='Soma dos Números' blue>
       <div>
         <span>
           <span>Resultado:</span>
-          <strong> 10</strong>
+          <strong> { max + min}</strong>
         </span>
       </div>
     </Card>
