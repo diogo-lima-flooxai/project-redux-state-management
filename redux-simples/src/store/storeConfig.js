646e702868e91f3f267aa163d5fd84e7efc9afe9
diogo <diogo.lima.flooxai@gmail.com>
2025-04-25 09:40:44 -0300

    feat: add Action Creator

--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -1,25 +1,34 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
-  numeros: function(state, action) {
-    console.log(state, ' ', action)
-    return { 
-      min: 7,
-      max: 31
+  numeros: function (state, action) {
+    switch (action.type) {
+      case "NUM_MIN_ALTERADO":
+        return {
+          ...state,
+          min: action.payload,
+        };
+      case "NUM_MAX_ALTERADO":
+        return {
+          ...state,
+          max: action.payload,
+        };
+      default:
+        return {
+          min: 7,
+          max: 31,
+        };
     }
   },
-  nomes: function(state, action){
-    console.log(state, ' ', action)
-    return [
-      'Ana',
-      'Bia',
-      'Carlos'
-    ]
-  }
-})
+  nomes: function (state, action) {
+    console.log("Reducer Nomes...");
+    console.log(state, " ", action);
+    return ["Ana", "Bia", "Carlos"];
+  },
+});
 
 function storeConfig() {
-  return createStore(reducers)
+  return createStore(reducers);
 }
 
-export default storeConfig
+export default storeConfig;
