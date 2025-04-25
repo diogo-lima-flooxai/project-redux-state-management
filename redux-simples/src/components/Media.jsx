8441135dbaf8841fb58e91fc9d4efbe83f40b605
diogo <diogo.lima.flooxai@gmail.com>
2025-04-25 07:47:13 -0300

    feat: add exemplary components

---
+++ redux-simples/src/components/Media.jsx
@@ -0,0 +1,14 @@
+import Card from './Card'
+
+export default props => {
+  return (
+    <Card title='Média dos Números' green>
+      <div>
+        <span>
+          <span>Resultado:</span>
+          <strong> 5</strong>
+        </span>
+      </div>
+    </Card>
+  )
+}
